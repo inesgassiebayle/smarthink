daf2ad9f5a64b3ec0df6d52be40453d6577216f0
ClaraLopez1 <mclopez@mail.austral.edu.ar>
2024-11-20 16:02:01 -0300

    edit profile

--- src/common/UserProfile/UserProfile.tsx
+++ src/common/UserProfile/UserProfile.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from "react";
 import background from "../../assets/Background.svg";
 import Avatar from "../Avatar/Avatar";
+import Icon from "../Icon/Icon";
 
 export interface UserProfileProps {
     name: string;
@@ -15,14 +16,21 @@ const UserProfile: FunctionComponent<UserProfileProps> = ({
         <div className="flex w-full flex-col items-center">
             <div
                 className="w-full h-[104px] bg-cover bg-center"
-                style={{ backgroundImage: `url(${background})` }}
+                style={{backgroundImage: `url(${background})`}}
             />
 
             <div className="flex flex-col items-center -mt-[56px]">
-                <Avatar imageSrc={avatarSrc} size="xlarge" />
-                <p className="text-center text-gray-800 font-medium mt-2 max-w-[200px] truncate">
-                    @{name}
-                </p>
+                <Avatar imageSrc={avatarSrc} size="xlarge"/>
+                <div className="flex items-center mt-2 max-w-[200px]">
+                    <p className="text-center text-gray-800 font-medium truncate">
+                        @{name}
+                    </p>
+                    <Icon
+                        variant="pencil"
+                        size="medium"
+                        colorClass="text-primary-500 pl-2"
+                    />
+                </div>
             </div>
         </div>
     );
